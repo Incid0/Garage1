5ff502440e6e451d21fbfb00cd1ac75f5afcb130
Peter Nilsson <incido@gmail.com>
2017-05-17 03:19:15 +0200

    Adding Vehicle method changed name.

--- Garage1/Program.cs
+++ Garage1/Program.cs
@@ -8,9 +8,9 @@ namespace Garage1 {
 	class Program {
 		static void Main(string[] args) {
 			var GarageBas = new Garage<Vehicle>(6);
-			GarageBas.AddVehicle(new Vehicle() { RegNr = "ABC123", Wheels = 4 });
-			GarageBas.AddVehicle(new Vehicle() { RegNr = "XXX666", Wheels = 2 });
-			GarageBas.AddVehicle(new Vehicle() { RegNr = "TXT000", Wheels = 10 });
+			GarageBas.Add(new Vehicle() { RegNr = "ABC123", Wheels = 4 });
+			GarageBas.Add(new Vehicle() { RegNr = "XXX666", Wheels = 2 });
+			GarageBas.Add(new Vehicle() { RegNr = "TXT000", Wheels = 10 });
 
 			foreach(var vehicle in GarageBas) {
 				Console.WriteLine(vehicle.RegNr);
